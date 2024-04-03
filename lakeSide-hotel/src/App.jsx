a22066bf54a773aaaec6165d6ad07c5fa25dcd92
Mgokayay <aymustafagokay@gmail.com>
2024-04-03 19:45:34 +0300

    routing setted

--- lakeSide-hotel/src/App.jsx
+++ lakeSide-hotel/src/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
-import AddRoom from "./components/room/AddRoom";
+
+import ExistingRoom from "./components/room/ExistingRoom.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home.jsx";
+import EditRoom from "./components/room/EditRoom.jsx";
 
 function App() {
   return (
     <>
-      <AddRoom />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-room" element={<ExistingRoom />} />
+          </Routes>
+        </Router>
+      </main>
     </>
   );
 }

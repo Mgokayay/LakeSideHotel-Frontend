dd0eeda1690c1ca8d39d3b75a36a5880f6b6c0ee
Mgokayay <aymustafagokay@gmail.com>
2024-04-06 21:08:46 +0300

    addRoom and navbar added to the app

--- lakeSide-hotel/src/App.jsx
+++ lakeSide-hotel/src/App.jsx
@@ -6,16 +6,20 @@ import ExistingRoom from "./components/room/ExistingRoom.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 import EditRoom from "./components/room/EditRoom.jsx";
+import AddRoom from "./components/room/AddRoom.jsx";
+import Navbar from "./components/layout/Navbar.jsx";
 
 function App() {
   return (
     <>
       <main>
         <Router>
+          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-room" element={<ExistingRoom />} />
+            <Route path="/add-room" element={<AddRoom />} />
           </Routes>
         </Router>
       </main>
